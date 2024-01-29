694a0c0fbab968bc66dc15a1edf8461f51873420
Ahmad Alsultan <ahmadlp.sn@Icloud.com>
2024-01-29 21:42:47 +0100

    fix key names

--- startup-script.sh
+++ startup-script.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Fetch secrets from Google Cloud Secret Manager
-OPENAI_API_KEY=$(gcloud secrets versions access latest --secret=openai-api-key)
-OPENAI_MODEL_ID=$(gcloud secrets versions access latest --secret=openai-model-id)
-STORAGE_TYPE=$(gcloud secrets versions access latest --secret=storage-type)
-GCP_STORAGE_BUCKET=$(gcloud secrets versions access latest --secret=gcp-storage-bucket)
-GCP_PROJECT_ID=$(gcloud secrets versions access latest --secret=gcp-project-id)
-GCP_SA_KEY=$(gcloud secrets versions access latest --secret=gcp-sa-key)
+OPENAI_API_KEY=$(gcloud secrets versions access latest --secret=OPENAI_API_KEY)
+OPENAI_MODEL_ID=$(gcloud secrets versions access latest --secret=OPENAI_MODEL_ID)
+STORAGE_TYPE=$(gcloud secrets versions access latest --secret=STORAGE_TYPE)
+GCP_STORAGE_BUCKET=$(gcloud secrets versions access latest --secret=GCP_STORAGE_BUCKET)
+GCP_PROJECT_ID=$(gcloud secrets versions access latest --secret=GCP_PROJECT_ID)
+GCP_SA_KEY=$(gcloud secrets versions access latest --secret=GCP_SA_KEY)
 
 # Export secrets as environment variables
 export OPENAI_API_KEY OPENAI_MODEL_ID STORAGE_TYPE GCP_STORAGE_BUCKET GCP_PROJECT_ID GCP_SA_KEY
 
-node run build
-pm2 restart dist/app/index.js
+npm run build
+npm start
